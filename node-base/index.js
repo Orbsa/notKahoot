2e232a7a27f37159dbc44ee65b4d1584ab4604c6
BuildTools <unconfigured@null.spigotmc.org>
2018-09-19 16:03:43 -0600

    Setting up NodeJS

---
+++ node-base/index.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env nodejs
+var express = require('express');
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(3450, 'localhost');
+console.log('Server running at http://orbsa.net/3450/');
