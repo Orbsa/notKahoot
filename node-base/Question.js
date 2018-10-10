09e27bafedab3d9d66a5d6f5d57cda56e3197785
Eric Bell <orbsa789@gmail.com>
2018-10-10 15:53:39 -0600

    Add questions class

---
+++ node-base/Question.js
@@ -0,0 +1,14 @@
+// Question class
+// var cat = new Question('Question text') 
+// cat.answers.push(Answer3)
+// etc.
+//
+
+var Question= {
+    question: function (text) { 
+        this.text = text; //Name of quiz. 
+        this.answers=[]; //List of questions on quiz.
+    }
+};
+
+module.exports=Question;
