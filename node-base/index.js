36355671911de80a823cba1a14e50ebb82e8ee7d
andydmort <andydmort@gmail.com>
2018-10-10 20:48:20 -0600

    Added a endpoint for the createQuestion.html

--- node-base/index.js
+++ node-base/index.js
@@ -5,4 +5,8 @@ app.get("/",function(req,res){
 	res.sendFile(__dirname + "/home.html");
 })
 
+app.get("/createQuiz",function(req,res){
+	res.sendFile(__dirname + "/createQuiz.html");
+})
+
 http.listen(3450, ()=>console.log("Server at Localhost:3450"));
